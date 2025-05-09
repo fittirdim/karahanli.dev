4dc3f58a1d14935a31ba8bfe9dc580d4c722a533
mete <132741294+fittirdim@users.noreply.github.com>
2025-05-09 15:27:34 +0300

    Delete karahanli-main directory

--- karahanli-main/src/app/shared-metadata.ts
+++
@@ -1,8 +0,0 @@
-export const sharedTitle = "tuna.one";
-export const sharedDescription =
-  "Self taught full-stack developer from Turkey. Making projects with 💓";
-export const sharedImage = {
-  width: 1200,
-  height: 630,
-  type: "image/png",
-};
